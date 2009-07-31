9981bf25ef48e2f9e6e2c10f5dae552e76b36f65
Stefan Langenmaier <stefan.langenmaier+github@gmail.com>
2009-07-31 13:01:23 +0000

    Änderungen zum auffinden von gleichen RuleActions ine inem Hash. Muss noch ausprobiert werden.

--- src/de/langenmaier/u2r3/RuleAction.java
+++ src/de/langenmaier/u2r3/RuleAction.java
@@ -10,6 +10,7 @@ import de.langenmaier.u2r3.rules.Rule;
 public class RuleAction {
 	private Rule rule = null;
 	private DeltaRelation delta = null;
+	private double weight = 0;
 
 	public RuleAction(Rule r) {
 		rule = r;
@@ -23,6 +24,32 @@ public class RuleAction {
 
 	public void apply() {
 		rule.apply(delta);
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof RuleAction) {
+			RuleAction ra = (RuleAction) obj;
+			if (delta.equals(ra.delta) && rule.equals(ra.rule)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	@Override
+	public int hashCode() {
+		//XXX koennte vllt noch besser gehen
+		//Annahme das Rule Objekte nur einmalig exisitieren und daher immer den selben HashCode haben
+		return rule.hashCode();
 	}
 
 }
