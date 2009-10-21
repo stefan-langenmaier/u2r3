b1c82002155f71acd6dddd64061682114622db32
Stefan Langenmaier <stefan.langenmaier+github@gmail.com>
2009-10-21 10:40:43 +0000

    an die Relationenveränderung angepasst

--- src/de/langenmaier/u2r3/owl/OWL2RLDBAdder.java
+++ src/de/langenmaier/u2r3/owl/OWL2RLDBAdder.java
@@ -122,7 +122,7 @@ public class OWL2RLDBAdder extends U2R3Component implements OWLAxiomVisitor {
 	@Override
 	public void visit(OWLObjectPropertyAssertionAxiom axiom) {
 		logger.debug("  adding ObjectPropertyAssertion:" + axiom.toString());
-		relationManager.getRelation(RelationName.propertyAssertionEnt).add(axiom);
+		relationManager.getRelation(RelationName.objectPropertyAssertion).add(axiom);
 		//ObjectPropertyAssertionRelation.getRelation().add(axiom);
 		logger.debug("  added ObjectPropertyAssertion");
 	}
@@ -182,7 +182,7 @@ public class OWL2RLDBAdder extends U2R3Component implements OWLAxiomVisitor {
 	@Override
 	public void visit(OWLDataPropertyAssertionAxiom axiom) {
 		logger.debug("  adding DataPropertyAssertionAxiom:" + axiom.toString());
-		relationManager.getRelation(RelationName.propertyAssertionLit).add(axiom);
+		relationManager.getRelation(RelationName.dataPropertyAssertion).add(axiom);
 		if (axiom.getObject().isTyped()) {
 			logger.error("AUCH den type hinzufügen!");
 		}
