e448803072ff026440d79111821a762836f60284
Stefan Langenmaier <stefan.langenmaier+github@gmail.com>
2009-11-03 13:53:15 +0000

    Testfall rdfbased-sem-class-thing-term bestanden

--- src/de/langenmaier/u2r3/tests/quality/fzitestcases/RunTestCase.java
+++ src/de/langenmaier/u2r3/tests/quality/fzitestcases/RunTestCase.java
@@ -44,7 +44,7 @@ public class RunTestCase {
 		
 		logger.info("Started Testcases");
 		
-		name = "rdfbased-sem-class-nothing-type";
+		name = "rdfbased-sem-class-thing-term";
 		//name = "rdfbased-sem-rdfs-domain-cond";
 		folder = folder + "/" + name;
 		runTestCase(name, folder, CheckType.entailment_check);
