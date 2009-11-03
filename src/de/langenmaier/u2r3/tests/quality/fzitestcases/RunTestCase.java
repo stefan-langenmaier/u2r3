22b0a60e09a41436b7942d953626afae6fd562e4
Stefan Langenmaier <stefan.langenmaier+github@gmail.com>
2009-11-03 16:04:21 +0000

    Testfall rdfbased-sem-eqdis-disclass-inst bestanden

--- src/de/langenmaier/u2r3/tests/quality/fzitestcases/RunTestCase.java
+++ src/de/langenmaier/u2r3/tests/quality/fzitestcases/RunTestCase.java
@@ -44,7 +44,7 @@ public class RunTestCase {
 		
 		logger.info("Started Testcases");
 		
-		name = "rdfbased-sem-eqdis-disclass-eqclass";
+		name = "rdfbased-sem-eqdis-disclass-inst";
 		//name = "rdfbased-sem-rdfs-domain-cond";
 		folder = folder + "/" + name;
 		runTestCase(name, folder, CheckType.consistency_check);
