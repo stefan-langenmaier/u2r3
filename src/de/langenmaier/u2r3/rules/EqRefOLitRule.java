6545ed625026d4adcb21019f0af34fcb8c6f37f0
Stefan Langenmaier <stefan.langenmaier+github@gmail.com>
2009-11-04 13:33:46 +0000

    * eq-ref-o-lit hinzugefügt Regel. Nachtrag

---
+++ src/de/langenmaier/u2r3/rules/EqRefOLitRule.java
@@ -0,0 +1,56 @@
+package de.langenmaier.u2r3.rules;
+
+import org.apache.log4j.Logger;
+
+import de.langenmaier.u2r3.core.U2R3Reasoner;
+import de.langenmaier.u2r3.db.DeltaRelation;
+import de.langenmaier.u2r3.db.RelationManager.RelationName;
+import de.langenmaier.u2r3.util.Settings.DeletionType;
+
+public class EqRefOLitRule extends ApplicationRule {
+	static Logger logger = Logger.getLogger(EqRefOLitRule.class);
+	
+	EqRefOLitRule(U2R3Reasoner reasoner) {
+		super(reasoner);
+		targetRelation = RelationName.sameAsLit;
+		
+		relationManager.getRelation(RelationName.dataPropertyAssertion).addAdditionRule(this);
+		
+		relationManager.getRelation(RelationName.sameAsLit).addDeletionRule(this);
+	}
+
+	@Override
+	protected String buildQuery(DeltaRelation delta, DeltaRelation newDelta,
+			boolean again, int run) {
+		StringBuilder sql = new StringBuilder(400);
+		
+		sql.append("INSERT INTO " + newDelta.getDeltaName());
+		
+		if (settings.getDeletionType() == DeletionType.CASCADING) {
+			sql.append(" (left, right, sourceId1, sourceTable1)");
+			sql.append("\n\t SELECT object AS left, object AS right,");
+			sql.append(" MIN(id) AS sourceId1, '" + RelationName.objectPropertyAssertion + "' AS sourceTable1");
+		} else {
+			sql.append("(left, right)");
+			sql.append("\n\t SELECT DISTINCT object AS left, object AS right");
+		}
+		
+		sql.append("\n\t FROM " + delta.getDeltaName() + " AS top");
+		
+		if (again) {
+			sql.append("\n\t WHERE NOT EXISTS (");
+			sql.append("\n\t\t SELECT left, right");
+			sql.append("\n\t\t FROM " + newDelta.getDeltaName() + " AS bottom");
+			sql.append("\n\t\t WHERE bottom.left = top.object AND bottom.right = top.object");
+			sql.append("\n\t )");
+		}
+		sql.append("\n\t GROUP BY object");
+		return sql.toString();
+	}
+
+	@Override
+	public String toString() {
+		return "sameAsLitt(A,A) :- dataPropertyAssertion(S, P, A)";
+	}
+
+}
