7d3f60c034f2d677b5f1e8ad1936d0bce59e9381
Stefan Langenmaier <stefan.langenmaier+github@gmail.com>
2009-11-04 11:39:55 +0000

    Testfall rdfbased-sem-eqdis-eqprop-sym bestanden

--- src/de/langenmaier/u2r3/tests/quality/fzitestcases/RunTestCase.java
+++ src/de/langenmaier/u2r3/tests/quality/fzitestcases/RunTestCase.java
@@ -44,7 +44,7 @@ public class RunTestCase {
 		
 		logger.info("Started Testcases");
 		
-		name = "rdfbased-sem-eqdis-eqprop-subst";
+		name = "rdfbased-sem-eqdis-eqprop-sym";
 		//name = "rdfbased-sem-rdfs-domain-cond";
 		folder = folder + "/" + name;
 		runTestCase(name, folder, CheckType.entailment_check);
