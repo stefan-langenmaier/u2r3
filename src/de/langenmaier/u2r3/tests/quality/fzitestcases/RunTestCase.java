635af96f66f85c30f15fc7e306ecdbe64ba2c812
Stefan Langenmaier <stefan.langenmaier+github@gmail.com>
2009-11-04 10:02:04 +0000

    Testfall rdfbased-sem-eqdis-eqclass-sym bestanden

--- src/de/langenmaier/u2r3/tests/quality/fzitestcases/RunTestCase.java
+++ src/de/langenmaier/u2r3/tests/quality/fzitestcases/RunTestCase.java
@@ -44,7 +44,7 @@ public class RunTestCase {
 		
 		logger.info("Started Testcases");
 		
-		name = "rdfbased-sem-eqdis-eqclass-subst";
+		name = "rdfbased-sem-eqdis-eqclass-sym";
 		//name = "rdfbased-sem-rdfs-domain-cond";
 		folder = folder + "/" + name;
 		runTestCase(name, folder, CheckType.entailment_check);
