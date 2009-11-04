d218254dca08a3aa478c17a131c2a941007d0f88
Stefan Langenmaier <stefan.langenmaier+github@gmail.com>
2009-11-04 13:40:04 +0000

    Testfall rdfbased-sem-eqdis-sameas-trans bestanden

--- src/de/langenmaier/u2r3/tests/quality/fzitestcases/RunTestCase.java
+++ src/de/langenmaier/u2r3/tests/quality/fzitestcases/RunTestCase.java
@@ -44,7 +44,7 @@ public class RunTestCase {
 		
 		logger.info("Started Testcases");
 		
-		name = "rdfbased-sem-eqdis-sameas-sym";
+		name = "rdfbased-sem-eqdis-sameas-trans";
 		//name = "rdfbased-sem-rdfs-domain-cond";
 		folder = folder + "/" + name;
 		runTestCase(name, folder, CheckType.entailment_check);
