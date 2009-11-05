aafd69ae049cc3ef52fb5455097e05855296e6c3
Stefan Langenmaier <stefan.langenmaier+github@gmail.com>
2009-11-05 16:22:17 +0000

    Testfall rdfbased-sem-rdfs-domain-cond bestanden

--- src/de/langenmaier/u2r3/tests/quality/fzitestcases/RunTestCase.java
+++ src/de/langenmaier/u2r3/tests/quality/fzitestcases/RunTestCase.java
@@ -44,7 +44,7 @@ public class RunTestCase {
 		
 		logger.info("Started Testcases");
 		
-		name = "rdfbased-sem-prop-comment-type";
+		name = "rdfbased-sem-rdfs-domain-cond";
 		//name = "rdfbased-sem-rdfs-domain-cond";
 		folder = folder + "/" + name;
 		runTestCase(name, folder, CheckType.entailment_check);
