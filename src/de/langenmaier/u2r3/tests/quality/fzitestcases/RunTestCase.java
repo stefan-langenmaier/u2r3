2d5097f0ae985a2e6733149b055c022257a99d8a
Stefan Langenmaier <stefan.langenmaier+github@gmail.com>
2009-11-06 15:24:08 +0000

    Testfall rdfbased-sem-simple-emptygraph-any bestanden

--- src/de/langenmaier/u2r3/tests/quality/fzitestcases/RunTestCase.java
+++ src/de/langenmaier/u2r3/tests/quality/fzitestcases/RunTestCase.java
@@ -44,7 +44,7 @@ public class RunTestCase {
 		
 		logger.info("Started Testcases");
 		
-		name = "rdfbased-sem-rdfsext-range-superclass";
+		name = "rdfbased-sem-simple-emptygraph-any";
 		//name = "rdfbased-sem-rdfs-domain-cond";
 		folder = folder + "/" + name;
 		runTestCase(name, folder, CheckType.entailment_check);
