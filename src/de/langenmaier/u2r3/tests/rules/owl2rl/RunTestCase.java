3b57a711b7295af7a689eeb0f775434b63b18337
Stefan Langenmaier <stefan.langenmaier+github@gmail.com>
2009-11-24 11:12:52 +0000

    Alle Testfälle laufen lassen und die Testmethode verbessert, damit sie auch gut von anderen benützt werden kann.

--- src/de/langenmaier/u2r3/tests/rules/owl2rl/RunTestCase.java
+++ src/de/langenmaier/u2r3/tests/rules/owl2rl/RunTestCase.java
@@ -1,6 +1,7 @@
 package de.langenmaier.u2r3.tests.rules.owl2rl;
 
 import java.io.File;
+import java.io.FileFilter;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URI;
@@ -20,7 +21,6 @@ import org.semanticweb.owlapi.model.OWLOntologyManager;
 import de.langenmaier.u2r3.core.U2R3Reasoner;
 import de.langenmaier.u2r3.core.U2R3ReasonerFactory;
 import de.langenmaier.u2r3.tests.rules.AxiomChecker;
-import de.langenmaier.u2r3.tests.rules.CheckType;
 import de.langenmaier.u2r3.util.Settings.DeltaIteration;
 
 public class RunTestCase {
@@ -37,15 +37,80 @@ public class RunTestCase {
 		BasicConfigurator.configure();
 		Logger.getRootLogger().setLevel(Level.INFO);
 		
-		if (args.length<=2) {
-			System.err.println("USAGE: java " + RunTestCase.class.getName() + " <folder> <name> <number>");
+		if (args.length<=0) {
+			System.err.println("USAGE: java " + RunTestCase.class.getName() + " <folder> [<name> [<number>]]");
 			return;
 		}
-		String folder = args[0];
-		String name = args[1];
-		String number = args[2];
 		
-		String path = folder + "/" + name + "/" + number + ".owl";
+		String folder = null;
+		String name = null;
+		String number = null;
+		
+		if (args.length >= 1) {
+			folder = args[0];
+		}
+		if (args.length >= 2) {
+			name = args[1];
+		}
+		if (args.length >= 3) {
+			number = args[2];
+		}
+		
+		if (args.length == 1) {
+			runTestCase(folder);
+		}
+		if (args.length == 2) {
+			runTestCase(folder, name);
+		}
+		if (args.length == 3) {
+			runTestCase(folder, name, number);
+		}
+	}
+	
+	public static void runTestCase(String folder) {
+		File dir = new File(folder);
+		
+		// This filter only returns directories
+		FileFilter fileFilter = new FileFilter() {
+			public boolean accept(File file) {
+				return file.isDirectory() && !file.isHidden();
+			}
+		};
+		
+		File[] files = dir.listFiles(fileFilter);
+		
+		for (File f : files) {
+			runTestCase(folder, f.getName());
+		}
+		
+	}
+	
+	public static void runTestCase(String folder, String name) {
+		File dir = new File(folder + "/" + name);
+		
+		// This filter only returns directories
+		FileFilter fileFilter = new FileFilter() {
+			public boolean accept(File file) {
+				boolean isInt = true;
+				try {
+					Integer.parseInt(file.getName());
+				} catch (NumberFormatException ex) {
+					isInt = false;
+				}
+				return file.isDirectory() && !file.isHidden() && isInt;
+			}
+		};
+		
+		File[] files = dir.listFiles(fileFilter);
+		
+		for (File f : files) {
+			runTestCase(folder, name, f.getName());
+		}
+		
+	}
+	
+	public static void runTestCase(String folder, String name, String number) {
+		String path = folder + "/" + name + "/" + number +"/" + number + ".owl";
 		
 		File f = new File(path);
 		
@@ -54,13 +119,12 @@ public class RunTestCase {
 		} else {
 			runConsistencyTestCase(folder, name, number);
 		}
-		//runTestCase(folder, name, number, type);
 	}
 	
 	public static void runEntailmentTestCase(String folder, String name, String number) {
 		try {
 			
-			folder = "file://" + folder + "/" + name + "/";
+			folder = "file://" + folder + "/" + name + "/" + number + "/";
 			String premise_uri = folder + number + ".premise.owl";
 			String conclusion_uri = folder + number + ".conclusion.owl";
 			
@@ -73,7 +137,7 @@ public class RunTestCase {
 			OWLReasonerFactory reasonerFactory = new U2R3ReasonerFactory();
 			U2R3Reasoner reasoner = (U2R3Reasoner) reasonerFactory.createReasoner(manager, null);
 			reasoner.getSettings().setDeltaIteration(DeltaIteration.COLLECTIVE);
-			//reasoner.getSettings().checkProfile(false);
+			reasoner.getSettings().checkProfile(false);
 			reasoner.loadOntologies(Collections.singleton(premise));
 		
 			reasoner.classify();
@@ -105,7 +169,7 @@ public class RunTestCase {
 	public static void runConsistencyTestCase(String folder, String name, String number) {
 		try {
 			
-			folder = "file://" + folder + "/" + name + "/";
+			folder = "file://" + folder + "/" + name + "/" + number + "/";
 			String ont_uri = folder +  number + ".owl";
 
 			OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
@@ -135,58 +199,5 @@ public class RunTestCase {
 		}
 	}
 	
-	public static void runTestCase(String folder, String name, String number, CheckType checkType) {
-		try {
-			
-			folder = "file://" + folder + "/";
-			String premise_uri = folder +  name + ".premisegraph.ttl";
-			if (checkType == CheckType.consistency_check) {
-				premise_uri = folder + name + ".graph.ttl";
-			}
-			String conclusion_uri = folder + name + ".conclusiongraph.ttl";
-			
-			OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
-			
-			OWLOntology premise;
-			premise = manager.loadOntologyFromPhysicalURI(URI.create(premise_uri));
-			logger.debug("Loaded " + premise.getOntologyID());
-			
-			OWLReasonerFactory reasonerFactory = new U2R3ReasonerFactory();
-			U2R3Reasoner reasoner = (U2R3Reasoner) reasonerFactory.createReasoner(manager, null);
-			reasoner.getSettings().setDeltaIteration(DeltaIteration.COLLECTIVE);
-			//reasoner.getSettings().checkProfile(false);
-			reasoner.loadOntologies(Collections.singleton(premise));
-		
-			reasoner.classify();
-
-			if (checkType == CheckType.entailment_check) {
-				OWLOntology conclusion;
-				conclusion = manager.loadOntologyFromPhysicalURI(URI.create(conclusion_uri));
-				logger.debug("Loaded " + conclusion.getOntologyID());
-				
-				AxiomChecker axiomChecker = new AxiomChecker(reasoner);
-				for(OWLAxiom ax : conclusion.getAxioms()) {
-					ax.accept(axiomChecker);
-				}
-				
-				if (!axiomChecker.isCorrect()) {
-					logger.error("Fehler in Testfall <" + name + ">!");
-				} else {
-					logger.info("Tesfall <" + name + "> okay.");
-				}
-			} else {
-				if (!reasoner.isConsistent(premise)) {
-					logger.info("Tesfall <" + name + "> okay.");
-				} else {
-					logger.error("Fehler in Testfall <" + name + ">!");
-				}
-			}
-			
-		} catch (OWLOntologyCreationException e) {
-			e.printStackTrace();
-		} catch (OWLReasonerException e) {
-			e.printStackTrace();
-		}
-	}
 
 }
