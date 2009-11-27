2ef67aa91b5dc75486d3f7588f561179fd666f96
Stefan Langenmaier <stefan.langenmaier+github@gmail.com>
2009-11-27 11:12:49 +0000

    In build.xml bei der Erstellung der Jar Datei den Datumsstempel entfernt. Dadurch konnten die Shell-Skripte vereinfacht werden.

--- loadreasoner.sh
+++ loadreasoner.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-CP=".:../import/h2-1.1.116.jar:../import/log4j-1.2.15.jar:../import/owlapi-bin.jar"
-cd build
+CP=".:./import/h2-1.1.116.jar:./import/log4j-1.2.15.jar:./import/owlapi-bin.jar:./dist/u2r3.jar"
 java -cp "$CP" de.langenmaier.u2r3.tests.util.LoadReasoner $1
